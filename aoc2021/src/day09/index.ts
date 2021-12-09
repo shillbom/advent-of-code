c85363c539a547376f01390ad8fd8381d0ea8f9b
Simon Hillbom <simon.hillbom@comhem.com>
2021-12-09 11:28:30 +0100

    day 9

--- aoc2021/src/day09/index.ts
+++ aoc2021/src/day09/index.ts
@@ -46,52 +46,52 @@ const part1 = (rawInput: string) => {
   return res.reduce((acc, f) => acc + f.value, 0);
 }
 
-function get2(x: number, y: number, matrix: number[][]) {
-  let ret = undefined;
-  try {
-    ret = matrix[y][x];
-  } catch {
-    // nop
-  }
-
-  ret = ret ?? 10
-  return ret
+interface pos {
+  x: number,
+  y: number,
+  val: number
 }
 
-function isLow2(x: number, y: number, matrix: number[][]) {
+let checked: pos[] = [];
+function isLow2(x: number, y: number, matrix: number[][]): pos[] {
   const min = matrix[y][x]
-  if (get2(x, y + 1, matrix) <= min) {
-    return false
+  if (checked.find(p => p.x == x && p.y == y)) {
+    return [];
   }
-  if (get2(x, y - 1, matrix) <= min) {
-    return false
-  }  
-  if (get2(x + 1, y, matrix) <= min) {
-    return false
-  }  
-  if (get2(x -1, y, matrix) <= min) {
-    return false
-  }
+  var ret: pos[] = [{x, y, val: min}]
+  checked.push({x, y, val: min})
 
-  return true;
+  let toVisit = [{x: x, y: y + 1},{x: x, y: y - 1},{x: x + 1, y: y},{x: x - 1, y: y}]
+  toVisit = toVisit.filter(pos => checked.find(c => c.x == pos.x && c.y == pos.y) == null)
+  for (let v of toVisit) {
+    var val = get(v.x, v.y, matrix);
+    if (val < 9) {
+      ret.push(...isLow2(v.x, v.y, matrix))
+    }
+  }
+  return ret;
 }
 
-function findBasin(x: number, y: number, matrix: number[][]) {
-  
+function findBasin(x: number, y: number, matrix: number[][]): number[] {
+  var here = get(x, y, matrix);
+  if (here == 9) {
+    return []
+  }
+  var ret = isLow2(x, y, matrix)
+  return ret.map(n => n.val)
 }
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput)
-  var res = []
+  var basins: number[][] = []
   for (let y = 0; y < input.length; y++) {
     for (let x = 0; x < input[y].length; x++) {
-      if (isLow(x, y, input)) {
-        res.push({x: x + 1 , y: y + 1, value: input[y][x] + 1});
-      }
+      basins.push(findBasin(x, y, input));
     }
   }
-  //console.log("result", res);
-  return res.reduce((acc, f) => acc + f.value, 0);
+
+  basins.sort((a, b) => b.length - a.length);
+  return (basins[0].length * basins[1].length * basins[2].length);
 }
 
 run({
